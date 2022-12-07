e54b52a594e2c0b96f29112869dda9a9f9603b96
DmitryKnyazkov <kde.2015@yandex.ru>
2022-12-07 20:10:55 +0300

    LR3_1_1 Done

--- Занятие3/Лабораторные_задания/task1_1/main.py
+++ Занятие3/Лабораторные_задания/task1_1/main.py
@@ -2,8 +2,10 @@ INPUT_FILE = "input.txt"
 
 
 def task() -> None:
-    with ... as file:  # TODO открыть указатель на файл
-        ...  # TODO файл является итератором, который работает с циклом for в построчном режиме
+    with open(INPUT_FILE) as file:      # TODO открыть указатель на файл
+        for str_ in file:
+            #str_ = str_.strip()
+            print(str_, end="")        # TODO файл является итератором, который работает с циклом for в построчном режиме
 
 
 if __name__ == "__main__":
