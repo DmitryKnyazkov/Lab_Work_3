29ed8797398446ff5cd6ca0c1200ef260ba602f1
DmitryKnyazkov <kde.2015@yandex.ru>
2022-12-08 16:44:19 +0300

    LR3_2_4 Done

--- Занятие3/Лабораторные_задания/task2_4/main.py
+++ Занятие3/Лабораторные_задания/task2_4/main.py
@@ -3,9 +3,10 @@ import json
 
 def task():
     filename = "input.json"
-    # TODO считать содержимое JSON файла
+    with open(filename) as fn:
+        words = json.load(fn)     # TODO считать содержимое JSON файла
 
-    gen_exr = ...  # TODO записать выражение-генератор возвращающее значение по ключу contains_improvement_appeals
+    gen_exr =  (x['contains_improvement_appeals'] for x in words)    # TODO записать выражение-генератор возвращающее значение по ключу contains_improvement_appeals
     return sum(gen_exr)
 
 
