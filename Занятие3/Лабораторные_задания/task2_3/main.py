2cdc1f7cb3e4b9b3d3e6963e236f2d557c90e0fc
DmitryKnyazkov <kde.2015@yandex.ru>
2022-12-08 16:08:02 +0300

    LR3_2_3 Done

--- Занятие3/Лабораторные_задания/task2_3/main.py
+++ Занятие3/Лабораторные_задания/task2_3/main.py
@@ -3,9 +3,13 @@ import json
 
 def task():
     filename = "input.json"
+    with open(filename) as fn:
+        dict_ = json.load(fn)
+        max_v = max(dict_, key=lambda x: x["score"])
+
     # TODO считать содержимое JSON файла
 
-    return ...  # TODO найти максимальный элемент по ключу score
+    return max_v     # TODO найти максимальный элемент по ключу score
 
 
 if __name__ == "__main__":
